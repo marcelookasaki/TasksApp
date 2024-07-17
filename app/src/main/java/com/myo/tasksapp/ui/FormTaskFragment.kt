1c52e7dda3e7bf541b2cb5c3523c957417a4a9f6
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2024-07-17 19:23:44 -0300

    Criando Layout Form Task

---
+++ app/src/main/java/com/myo/tasksapp/ui/FormTaskFragment.kt
@@ -0,0 +1,29 @@
+package com.myo.tasksapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.myo.tasksapp.R
+import com.myo.tasksapp.databinding.FragmentFormTaskBinding
+import com.myo.tasksapp.databinding.FragmentHomeBinding
+
+
+class FormTaskFragment : Fragment() {
+
+    private var _binding: FragmentFormTaskBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFormTaskBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
