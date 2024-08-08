317d3805d463d0cf28289a7a43ac6bc25c865aa2
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2024-08-08 00:21:05 -0300

    Evento de clique no RecyclerView

--- app/src/main/java/com/myo/tasksapp/ui/adapter/TaskAdapter.kt
+++ app/src/main/java/com/myo/tasksapp/ui/adapter/TaskAdapter.kt
@@ -13,9 +13,17 @@ import com.myo.tasksapp.databinding.ItemTaskBinding
 
 class TaskAdapter(
     private val context: Context,
-    private val taskList: List<Task>
+    private val taskList: List<Task>,
+    private val taskSelected: (Task, Int) -> Unit
 ) : RecyclerView.Adapter<TaskAdapter.MyViewHolder>() {
 
+    companion object {
+        val SELECT_PREVIOUS: Int = 1
+        val SELECT_REMOVE: Int = 2
+        val SELECT_EDIT: Int = 3
+        val SELECT_DETAILS: Int = 4
+        val SELECT_NEXT: Int = 5
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
         return MyViewHolder(ItemTaskBinding.inflate(
@@ -30,15 +38,20 @@ class TaskAdapter(
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val task = taskList[position]
 
-        holder.binding.tvDescription.text = task.descriptin
+        holder.binding.tvDescription.text = task.description
 
         setIndicator(task, holder)
+
+        holder.binding.btnDelete.setOnClickListener { taskSelected(task, SELECT_REMOVE) }
+        holder.binding.btnEdit.setOnClickListener { taskSelected(task, SELECT_EDIT) }
+        holder.binding.btnDetails.setOnClickListener { taskSelected(task, SELECT_DETAILS) }
     }
 
     private fun setIndicator(task: Task, holder: MyViewHolder) {
         when(task.status) {
             Status.TODO -> {
                 holder.binding.ibPrevious.isVisible = false
+                holder.binding.ibNext.setOnClickListener { taskSelected(task, SELECT_NEXT) }
             }
             Status.DOING -> {
                 holder.binding.ibPrevious.setColorFilter(
@@ -47,9 +60,13 @@ class TaskAdapter(
                 holder.binding.ibNext.setColorFilter(
                     ContextCompat.getColor(context, R.color.color_status_done)
                 )
+                holder.binding.ibPrevious.setOnClickListener { taskSelected(task, SELECT_PREVIOUS) }
+                holder.binding.ibNext.setOnClickListener { taskSelected(task, SELECT_NEXT) }
             }
             Status.DONE -> {
                 holder.binding.ibNext.isVisible = false
+                holder.binding.ibPrevious.setOnClickListener { taskSelected(task, SELECT_PREVIOUS) }
+
             }
         }
     }
