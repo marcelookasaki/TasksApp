89f9791a5293d6b1ab37fdd03f34bf7a43db1a5e
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2024-10-19 23:29:54 -0300

    RecyclerView

--- app/src/main/java/com/example/tasksapp/ui/DoingFragment.kt
+++ app/src/main/java/com/example/tasksapp/ui/DoingFragment.kt
@@ -5,13 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tasksapp.data.model.Status
 import com.example.tasksapp.databinding.FragmentDoingBinding
+import com.myo.tasksapp.data.model.Task
+import com.myo.tasksapp.ui.adapter.TaskAdapter
 
 
 class DoingFragment : Fragment() {
+
     private var _binding: FragmentDoingBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var taskAdapter: TaskAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,6 +25,24 @@ class DoingFragment : Fragment() {
         _binding = FragmentDoingBinding.inflate(inflater, container, false)
         return binding.root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView(getTasks())
+    }
+    private fun initRecyclerView(taskList: List<Task>) {
+        taskAdapter = TaskAdapter(taskList)
+        binding.rvTasksDoing.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvTasksDoing.setHasFixedSize(true)
+        binding.rvTasksDoing.adapter = taskAdapter
+    }
+    private fun getTasks() = listOf<Task>(
+        Task("0","Fazendo o curso kotlin", Status.DOING),
+        Task("1","Atualizando os complentos", Status.DOING),
+        Task("2","Ajustando as atividades", Status.DOING),
+        Task("3","Planejamento ajuste do cardápio", Status.DOING),
+        Task("4","Ajustando os planejamentos", Status.DOING),
+        Task("5","Estudando espiritismo", Status.DOING)
+    )
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/tasksapp/ui/DoneFragment.kt
+++ app/src/main/java/com/example/tasksapp/ui/DoneFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tasksapp.data.model.Status
 import com.example.tasksapp.databinding.FragmentDoneBinding
+import com.myo.tasksapp.data.model.Task
+import com.myo.tasksapp.ui.adapter.TaskAdapter
 
 
 class DoneFragment : Fragment() {
+
     private var _binding: FragmentDoneBinding? = null
     private val binding get() = _binding!!
-
+    private lateinit var taskAdapter: TaskAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,6 +24,25 @@ class DoneFragment : Fragment() {
         _binding = FragmentDoneBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView(getTasks())
+    }
+    private fun initRecyclerView(taskList: List<Task>) {
+        taskAdapter = TaskAdapter(taskList)
+        binding.rvTasksDone.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvTasksDone.setHasFixedSize(true)
+        binding.rvTasksDone.adapter = taskAdapter
+    }
+    private fun getTasks() = listOf<Task>(
+        Task("0","Estudo o livro dos espíritos", Status.DONE),
+        Task("1","Pedir whey, tmz e creatina", Status.DONE),
+        Task("2","Ajustando as atividades", Status.DONE),
+        Task("3","Planejamento ajuste do cardápio", Status.DONE),
+        Task("4","Ajustando os planejamentos", Status.DONE),
+        Task("5","Estudando espiritismo", Status.DONE)
+    )
 
     override fun onDestroyView() {
         super.onDestroyView()
