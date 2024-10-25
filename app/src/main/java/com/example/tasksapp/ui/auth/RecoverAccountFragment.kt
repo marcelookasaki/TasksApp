e2d119d824007cce753fd5d5519720fd87570300
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2024-10-25 19:53:43 -0300

    UserAuthVerification

--- app/src/main/java/com/example/tasksapp/ui/auth/RecoverAccountFragment.kt
+++ app/src/main/java/com/example/tasksapp/ui/auth/RecoverAccountFragment.kt
@@ -6,15 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import com.example.tasksapp.R
 import com.example.tasksapp.databinding.FragmentRecoverAccountBinding
 import com.example.tasksapp.util.initToolBar
 import com.example.tasksapp.util.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RecoverAccountFragment : Fragment() {
 
     private var _binding: FragmentRecoverAccountBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +33,10 @@ class RecoverAccountFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolBar(binding.abMtbRaf)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
         initListeners()
     }
 
@@ -40,9 +50,16 @@ class RecoverAccountFragment : Fragment() {
         val email = binding.etEmailRaf.text.toString().trim()
 
         if (email.isNotEmpty()) {
+            binding.pbRaf.isVisible = true
             Toast.makeText(requireContext(), "OK!", Toast.LENGTH_LONG).show()
         }else {
             showBottomSheet(message = getString(R.string.email_empty))
+        }
+    }
+
+    private fun recoverUserAccount(email: String) {
+        auth.sendPasswordResetEmail(email).addOnFailureListener { task ->
+
         }
     }
 
