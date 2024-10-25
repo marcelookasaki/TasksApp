652cac482eb7f3a57c7730f34d47373c4667e89e
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2024-10-25 20:12:42 -0300

    UserLogout

--- app/src/main/java/com/example/tasksapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/tasksapp/ui/HomeFragment.kt
@@ -5,15 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.tasksapp.R
 import com.example.tasksapp.databinding.FragmentHomeBinding
 import com.example.tasksapp.ui.adapter.ViewPagerAdapter
+import com.example.tasksapp.util.showBottomSheet
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +33,26 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        initListeners()
         initTabs()
+    }
+
+    private fun initListeners() {
+        binding.ibExit.setOnClickListener {
+
+            showBottomSheet(
+                titleButton = R.string.text_btn_dialog_confirm_logout,
+                titleDialog = R.string.text_title_dialog_confirm_logout,
+                message = getString(R.string.text_message_dialog_confirm_logout),
+                onClick = {
+                    auth.signOut()
+                    findNavController().navigate(R.id.action_homeFragment_to_authentication)
+                }
+            )
+        }
     }
 
     private fun initTabs() {
